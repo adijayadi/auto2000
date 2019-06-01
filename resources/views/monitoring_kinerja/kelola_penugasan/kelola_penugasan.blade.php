add48f70e866915834f0b505585f78e2ad47be2c
foxbloodiron <ariakbar6944@gmail.com>
2019-06-01 16:02:48 +0700

    service advisor otw

--- resources/views/monitoring_kinerja/kelola_penugasan/kelola_penugasan.blade.php
+++ resources/views/monitoring_kinerja/kelola_penugasan/kelola_penugasan.blade.php
@@ -23,7 +23,7 @@
 </div>
 
 @include('monitoring_kinerja.kelola_penugasan.modal_gantiserviceadvisor')
-@include('monitoring_kinerja.kelola_penugasan.modal_gantiserviceadvisor_semua')
+{{-- @include('monitoring_kinerja.kelola_penugasan.modal_gantiserviceadvisor_semua') --}}
 
 <div class="wrapper wrapper-content animated fadeInRight">
     <div class="row">
@@ -33,7 +33,7 @@
                 <div class="ibox-title">
                     <h5>Kelola Penugasan</h5>
                     <div class="ibox-tools">
-                        <button class="btn btn-warning btn-sm" title="Ganti Service Advisor yang sudah tidak aktif" data-toggle="modal" data-target="#modal-ganti-semua">
+                        <button class="btn btn-warning btn-sm" title="Ganti Service Advisor yang sudah tidak aktif" data-toggle="modal" data-target="#modal-ganti-satu">
                             <i class="fa fa-edit"></i>
                             Ganti Service Advisor
                         </button>
@@ -63,11 +63,19 @@
                         </div>
                     </fieldset>
 
+                    <div class="text-right mb-3">
+                        <div class="btn-group btn-group-sm">
+                            <button class="btn btn-primary" type="button" id="btn-checkall">Check All</button>
+                            <button class="btn btn-default" type="button" id="btn-uncheckall">Uncheck All</button>
+                            <button class="btn btn-info" type="button" id="btn-interval">Check Interval</button>
+                        </div>
+                    </div>
+
                     <div class="table-responsive">
                         <table class="table table-striped table-bordered table-hover" id="table_kendaraan">
                             <thead>
                                 <tr>
-                                    <th width="1%">No.</th>
+                                    <th width="1%"></th>
                                     <th width="10%">Tanggal Service</th>
                                     <th>No. Rangka</th>
                                     <th>No. Polisi</th>
@@ -78,9 +86,11 @@
                                 </tr>
                             </thead>
                             <tbody>
-
+                                @for($i=0;$i<20;$i++)
                                 <tr>
-                                    <td>1</td>
+                                    <td>
+                                        <input type="checkbox" name="centang-followup[]" value="">
+                                    </td>
                                     <td>2018-10-01</td>
                                     <td>MHFM1BA2JBK035948</td>
                                     <td>B1295PKO</td>
@@ -93,6 +103,57 @@
                                         </button>
                                     </td>
                                 </tr>
+
+                                <tr>
+                                    <td>
+                                        <input type="checkbox" name="centang-followup" value="1">
+                                    </td>
+                                    <td>2018-10-01</td>
+                                    <td>MHFM1BA2JBK035948</td>
+                                    <td>B1295PKO</td>
+                                    <td>AVANZA</td>
+                                    <td>Service 10.000 Kilometer</td>
+                                    <td>Achmad Nur Taufik</td>
+                                    <td>
+                                        <button class="btn btn-warning btn-sm" title="Ganti Service Advisor per no. rangka" data-toggle="modal" data-target="#modal-ganti-satu">
+                                            <i class="fa fa-edit"></i>
+                                        </button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <input type="checkbox" name="centang-followup" value="2">
+                                    </td>
+                                    <td>2018-10-01</td>
+                                    <td>MR053AK50E4506151</td>
+                                    <td>L3PY</td>
+                                    <td>CAMRY</td>
+                                    <td> Service 50.000 Kilometer </td>
+                                    <td>Made Agung Adhi Gunawan</td>
+                                    <td>
+                                        <button class="btn btn-warning btn-sm" title="Ganti Service Advisor per no. rangka" data-toggle="modal" data-target="#modal-ganti-satu">
+                                            <i class="fa fa-edit"></i>
+                                        </button>
+                                    </td>
+                                </tr>
+
+                                <tr>
+                                    <td>
+                                        <input type="checkbox" name="centang-followup" value="3">
+                                    </td>
+                                    <td>2018-10-01</td>
+                                    <td>MHKM1BA2JDK041994</td>
+                                    <td>B1182BYK</td>
+                                    <td>AVANZA</td>
+                                    <td> Service 90.000 kilometer </td>
+                                    <td>Zainul Arifin</td>
+                                    <td>
+                                        <button class="btn btn-warning btn-sm" title="Ganti Service Advisor per no. rangka" data-toggle="modal" data-target="#modal-ganti-satu">
+                                            <i class="fa fa-edit"></i>
+                                        </button>
+                                    </td>
+                                </tr>
+                                @endfor
                             </tbody>
                         </table>
                     </div>
@@ -109,6 +170,42 @@
     $(document).ready(function(){
         $('#table_kendaraan').DataTable();
 
+
+    $('#btn-checkall').click(function(){
+        $('#table_kendaraan tbody [type="checkbox"]').prop('checked', true);
+    });
+    $('#btn-uncheckall').click(function(){
+        $('#table_kendaraan tbody [type="checkbox"]').prop('checked', false);
+    });
+
+    $('#btn-interval').click(function(){
+        var table = $('#table_kendaraan tbody [type="checkbox"]');
+        var count = table.length;
+        var range = [];
+        for(var i = 0; i<count;i++){
+            if (table.eq(i).is(':checked')) {
+                range.push(i);
+            }
+        }
+        console.log(range);
+        var range_l = range.length - 1;
+        var start = range[0];
+        var end = range[range_l];
+        console.log(range_l, start, end);
+        for(var j = start; j<end;  j++){
+            table.eq(j).prop('checked', true);
+        }
+    })
+
+    $('#table_kendaraan tbody > tr').click(function(e){
+        // console.log(e);
+
+        $(this).find('[type="checkbox"]').prop('checked', function(index, prop){
+
+            return prop == true ? false : true;
+        });
+
+    });
 
     });
 </script>
