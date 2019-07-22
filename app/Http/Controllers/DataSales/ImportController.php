f4579ecbc216a8b64bd482cc981341332466d1fe
FDhunterz <faizaltriswanto@gmail.com>
2019-07-22 15:56:04 +0700

    Update ImportController.php

--- app/Http/Controllers/DataSales/ImportController.php
+++ app/Http/Controllers/DataSales/ImportController.php
@@ -63,7 +63,6 @@ class ImportController extends Controller
           $code = $request->code;
          $check = DB::table('d_customerremovable')->count();
          if ($check == 0) {
-         dd($request->result['Sheet1'][$i][6] != null);
       		    $alldata = [];
                for ($i=1; $i < $request->datacount ; $i++) { 
                 if ($request->result['Sheet1'][$i][6] != null) {
